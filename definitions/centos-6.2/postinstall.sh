bd9bac0d0643f6f163807946177bc37e14bc8107
Sam Bailey <sam.bailey@alaress.com.au>
2012-06-07 10:01:17 +1000

    fixed puppetlabs-release repo install fixed /vagrant folder creation

--- definitions/centos-6.2/postinstall.sh
+++ definitions/centos-6.2/postinstall.sh
@@ -1,11 +1,9 @@
-#http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
-
 date > /etc/vagrant_box_build_time
 
 yum -y install gcc bzip2 make kernel-devel-`uname -r`
 yum -y update
 
-rpm -ivh http://yum.puppetlabs.com/el/6/products/\$basearch/puppetlabs-release-6-1.noarch.rpm
+rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-1.noarch.rpm
 
 yum -y update
 yum -y upgrade
@@ -24,6 +22,8 @@ chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chown -R vagrant /home/vagrant/.ssh
+
+mkdir -p /vagrant
 
 # Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
