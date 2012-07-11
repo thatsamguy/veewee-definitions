13c281446c5246e7225880ec947530534beefb87
Sam Bailey <sam.bailey@alaress.com.au>
2012-07-12 07:50:29 +1000

    added initial centos-6.3

---
+++ definitions/centos-6.3/postinstall.sh
@@ -0,0 +1,40 @@
+date > /etc/vagrant_box_build_time
+
+yum -y install gcc bzip2 make kernel-devel-`uname -r`
+yum -y update
+
+rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-5.noarch.rpm
+rpm -ivh http://linux.dell.com/dkms/permalink/dkms-2.2.0.3-1.noarch.rpm
+
+yum -y update
+yum -y upgrade
+
+yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite-devel
+yum -y install puppet facter ruby-devel rubygems
+yum -y erase  gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+yum -y clean all
+
+# Installing chef
+gem install --no-ri --no-rdoc chef
+
+# Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+# Installing the virtualbox guest additions
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+dd if=/dev/zero of=/tmp/clean || rm /tmp/clean
+
+exit
